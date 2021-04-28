b8cf162a2fa6df6367346daabaf052a072607abb
Alberto Ponces <ponces26@gmail.com>
2021-04-28 09:28:04 +0100

    Pixel Experience: AOSP-ify

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 
-## Building PHH-based LineageOS GSIs ##
+## Building PHH-based Pixel Experience GSIs ##
 
-To get started with building LineageOS GSI, you'll need to get familiar with [Git and Repo](https://source.android.com/source/using-repo.html) as well as [How to build a GSI](https://github.com/phhusson/treble_experimentations/wiki/How-to-build-a-GSI%3F).
+To get started with building Pixel Experience GSI, you'll need to get familiar with [Git and Repo](https://source.android.com/source/using-repo.html) as well as [How to build a GSI](https://github.com/phhusson/treble_experimentations/wiki/How-to-build-a-GSI%3F).
 
 First, open a new Terminal window, which defaults to your home directory.  Clone the modified treble_experimentations repo there:
 
-    git clone https://github.com/AndyCGYan/treble_experimentations
+    git clone https://github.com/ponces/treble_experimentations
 
-Create a new working directory for your LineageOS build and navigate to it:
+Create a new working directory for your Pixel Experience build and navigate to it:
 
-    mkdir lineage-18.x-build-gsi; cd lineage-18.x-build-gsi
+    mkdir pixel; cd pixel
 
-Initialize your LineageOS workspace:
+Initialize your Pixel Experience workspace:
 
-    repo init -u https://github.com/LineageOS/android.git -b lineage-18.1
+    repo init -u https://github.com/PixelExperience/manifest -b eleven
 
 Clone the modified treble patches and this repo:
 
-    git clone https://github.com/AndyCGYan/treble_patches -b lineage-18.1
-    git clone https://github.com/AndyCGYan/treble_build_los -b lineage-18.1
+    git clone https://github.com/ponces/treble_patches -b eleven
+    git clone https://github.com/ponces/treble_build_pe -b eleven
 
 Finally, start the build script:
 
-    bash treble_build_los/buildbot_treble.sh
+    bash treble_build_pe/buildbot_treble.sh
 
 Be sure to update the cloned repos from time to time!
 
--- buildbot_treble.sh
+++ buildbot_treble.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ""
-echo "LineageOS 18.x Treble Buildbot"
+echo "Pixel Experience 11 Treble Buildbot"
 echo "ATTENTION: this script syncs repo on each run"
 echo "Executing in 5 seconds - CTRL-C to exit"
 echo ""
@@ -8,11 +8,11 @@ sleep 5
 
 START=`date +%s`
 BUILD_DATE="$(date +%Y%m%d)"
-BL=$PWD/treble_build_los
+BL=$PWD/treble_build_pe
 
 echo "Preparing local manifest"
 mkdir -p .repo/local_manifests
-cp $BL/manifest.xml .repo/local_manifests/manifest.xml
+cp $BL/manifest.xml .repo/local_manifests/pixel.xml
 echo ""
 
 echo "Syncing repos"
@@ -23,14 +23,12 @@ echo "Setting up build environment"
 source build/envsetup.sh &> /dev/null
 echo ""
 
-repopick 289372 # Messaging: Add "Mark as read" quick action for message notifications
-
 echo "Reverting LOS FOD implementation"
 cd frameworks/base
 git am $BL/patches/0001-Squashed-revert-of-LOS-FOD-implementation.patch
 cd ../..
 cd frameworks/native
-git revert 381416d540ea92dca5f64cd48fd8c9dc887cac7b --no-edit # surfaceflinger: Add support for extension lib
+git revert 542069c3aa6d003887b4abba3c0f494e8271085f --no-edit # surfaceflinger: Add support for extension lib
 cd ../..
 echo ""
 
@@ -38,7 +36,8 @@ echo "Applying PHH patches"
 rm -f device/*/sepolicy/common/private/genfs_contexts
 cd device/phh/treble
 git clean -fdx
-bash generate.sh lineage
+cp $BL/pe.mk .
+bash generate.sh pe
 cd ../../..
 bash ~/treble_experimentations/apply-patches.sh treble_patches
 echo ""
@@ -48,62 +47,56 @@ cd build/make
 git am $BL/patches/0001-Make-broken-copy-headers-the-default.patch
 cd ../..
 cd frameworks/base
-git am $BL/patches/0001-UI-Revive-navbar-layout-tuning-via-sysui_nav_bar-tun.patch
 git am $BL/patches/0001-UI-Disable-wallpaper-zoom.patch
 git am $BL/patches/0001-Disable-vendor-mismatch-warning.patch
 cd ../..
-cd lineage-sdk
-git am $BL/patches/0001-sdk-Invert-per-app-stretch-to-fullscreen.patch
-cd ..
-cd packages/apps/LineageParts
-git am $BL/patches/0001-LineageParts-Invert-per-app-stretch-to-fullscreen.patch
-cd ../../..
-cd packages/apps/Trebuchet
-git am $BL/patches/0001-Trebuchet-Move-clear-all-button-to-actions-view.patch
-cd ../../..
-cd vendor/lineage
+cd vendor/aosp
 git am $BL/patches/0001-vendor_lineage-Log-privapp-permissions-whitelist-vio.patch
 cd ../..
 echo ""
 
 echo "Applying GSI-specific patches"
 cd bootable/recovery
-git revert 0e369f42b82c4d12edba9a46dd20bee0d4b783ec --no-edit # recovery: Allow custom bootloader msg offset in block misc
-cd ../..
-cd build/make
-git am $BL/patches/0001-build-Don-t-handle-apns-conf.patch
-git revert 78c28df40f72fdcbe3f82a83828060ad19765fa1 --no-edit # mainline_system: Exclude vendor.lineage.power@1.0 from artifact path requirements
+git revert c9a3611b0bab1744b3c4321e728c917fcdc2abc3 --no-edit # recovery: Allow custom bootloader msg offset in block misc
 cd ../..
 cd device/phh/treble
 git revert 82b15278bad816632dcaeaed623b569978e9840d --no-edit # Update lineage.mk for LineageOS 16.0
 git am $BL/patches/0001-Remove-fsck-SELinux-labels.patch
-git am $BL/patches/0001-treble-Add-overlay-lineage.patch
-git am $BL/patches/0001-treble-Don-t-specify-config_wallpaperCropperPackage.patch
+git am $BL/patches/0001-base-provide-libnfc-nci.patch
+git am $BL/patches/0001-base-remove-securize-script.patch
+git am $BL/patches/0001-board-add-broken-duplicate-rules-flag.patch
 cd ../../..
 cd frameworks/av
-git revert 5a5606dbd92f01de322c797a7128fce69902d067 --no-edit # camera: Allow devices to load custom CameraParameter code
+git revert 72fb8d96c85fd45e85516b4023cd5116b5d5a8eb --no-edit # camera: Allow devices to load custom CameraParameter code
 cd ../..
 cd frameworks/native
 git revert 581c22f979af05e48ad4843cdfa9605186d286da --no-edit # Add suspend_resume trace events to the atrace 'freq' category.
 cd ../..
 cd packages/apps/Bluetooth
-git revert 4ceb47e32c1be30640e40f81b6f741942f8598ed --no-edit # Bluetooth: Reset packages/apps/Bluetooth to upstream
+git revert bba4192627ca9987c0128f9774d79ffb17ece2f5 --no-edit # Bluetooth: Reset packages/apps/Bluetooth to upstream
 cd ../../..
 cd system/core
 git am $BL/patches/0001-Revert-init-Add-vendor-specific-initialization-hooks.patch
 git am $BL/patches/0001-Panic-into-recovery-rather-than-bootloader.patch
-git am $BL/patches/0001-Restore-sbin.patch
 git am $BL/patches/0001-fix-offline-charger-v7.patch
 cd ../..
 cd system/hardware/interfaces
-git revert cb732f9b635b5f6f79e447ddaf743ebb800b8535 --no-edit # system_suspend: start early
+git am $BL/patches/0001-Revert-system_suspend-start-early.patch
 cd ../../..
 cd system/sepolicy
 git am $BL/patches/0001-Revert-sepolicy-Relabel-wifi.-properties-as-wifi_pro.patch
 cd ../..
-cd vendor/lineage
+cd vendor/aosp
 git am $BL/patches/0001-build_soong-Disable-generated_kernel_headers.patch
+git am $BL/patches/0001-build-fix-build-number.patch
 cd ../..
+echo ""
+
+echo "Applying GSI-specific fixes"
+mkdir -p device/generic/common/nfc
+curl "https://android.googlesource.com/device/generic/common/+/refs/tags/android-11.0.0_r35/nfc/libnfc-nci.conf?format=TEXT"| base64 --decode > device/generic/common/nfc/libnfc-nci.conf
+mkdir -p device/sample/etc
+cp vendor/aosp/prebuilt/common/etc/apns-conf.xml device/sample/etc/apns-full-conf.xml
 echo ""
 
 echo "CHECK PATCH STATUS NOW!"
@@ -111,21 +104,45 @@ sleep 5
 echo ""
 
 export WITHOUT_CHECK_API=true
-export WITH_SU=true
-mkdir -p ~/build-output/
+mkdir -p ~/builds
 
 buildVariant() {
     lunch ${1}-userdebug
     make installclean
     make -j$(nproc --all) systemimage
     make vndk-test-sepolicy
-    mv $OUT/system.img ~/build-output/lineage-18.1-$BUILD_DATE-UNOFFICIAL-${1}.img
+    buildSasImage $1
 }
 
-buildVariant treble_arm_bvS
-buildVariant treble_a64_bvS
-buildVariant treble_arm64_bvS
-ls ~/build-output | grep 'lineage'
+buildSasImage() {
+    cd sas-creator
+    case $1 in
+    "treble_a64_bvN")
+        bash lite-adapter.sh 32 $OUT/system.img
+        xz -c s.img -T0 > ~/builds/PixelExperience_arm32_binder64-ab-vndklite-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c $OUT/system.img -T0 > ~/builds/PixelExperience_arm32_binder64-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        ;;
+    "treble_arm_bvN")
+        bash run.sh 32 $OUT/system.img
+        xz -c s.img -T0 > ~/builds/PixelExperience_arm-aonly-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c $OUT/system.img -T0 > ~/builds/PixelExperience_arm-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        ;;
+    "treble_arm64_bvN")
+        bash run.sh 64 $OUT/system.img
+        xz -c s.img -T0 > ~/builds/PixelExperience_arm64-aonly-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        bash lite-adapter.sh 64 $OUT/system.img
+        xz -c s.img -T0 > ~/builds/PixelExperience_arm64-ab-vndklite-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c $OUT/system.img -T0 > ~/builds/PixelExperience_arm64-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        ;;
+    esac
+    rm -rf s.img
+    cd ..
+}
+
+buildVariant treble_arm_bvN
+buildVariant treble_a64_bvN
+buildVariant treble_arm64_bvN
+ls ~/builds | grep 'PixelExperience'
 
 END=`date +%s`
 ELAPSEDM=$(($(($END-$START))/60))
