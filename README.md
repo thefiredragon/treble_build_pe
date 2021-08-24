660a6a6dc11108673671a68830c2a8a2870d5944
Alberto Ponces <ponces26@gmail.com>
2021-08-24 09:25:38 +0100

    Improve patches handling

--- README.md
+++ README.md
@@ -3,26 +3,21 @@
 
 To get started with building Pixel Experience GSI, you'll need to get familiar with [Git and Repo](https://source.android.com/source/using-repo.html) as well as [How to build a GSI](https://github.com/phhusson/treble_experimentations/wiki/How-to-build-a-GSI%3F).
 
-First, open a new Terminal window, which defaults to your home directory.  Clone the modified treble_experimentations repo there:
-
-    git clone https://github.com/ponces/treble_experimentations
-
-Create a new working directory for your Pixel Experience build and navigate to it:
+First, create a new working directory for your Pixel Experience build and navigate to it:
 
     mkdir pixel; cd pixel
 
 Initialize your Pixel Experience workspace:
 
-    repo init -u https://github.com/PixelExperience/manifest -b eleven
+    repo init -u https://github.com/PixelExperience/manifest -b [eleven|eleven-plus]
 
-Clone the modified treble patches and this repo:
+Clone this repo:
 
-    git clone https://github.com/ponces/treble_patches -b eleven
-    git clone https://github.com/ponces/treble_build_pe -b eleven
+    git clone https://github.com/ponces/treble_build_pe -b [eleven|eleven-plus]
 
 Finally, start the build script:
 
-    bash treble_build_pe/buildbot_treble.sh
+    bash treble_build_pe/buildbot_treble.sh [eleven|eleven-plus]
 
 Be sure to update the cloned repos from time to time!
 
--- buildbot_treble.sh
+++ buildbot_treble.sh
@@ -19,6 +19,9 @@ echo\
 START=`date +%s`
 BUILD_DATE="$(date +%Y%m%d)"
 BL=$PWD/treble_build_pe
+BRANCH=$1
+[ "$BRANCH" == "" ] && BRANCH="eleven"
+[ "$BRANCH" == "eleven" ] && BUILD="PixelExperience" || BUILD="PixelExperience_Plus"
 
 echo "Preparing local manifest"
 mkdir -p .repo/local_manifests
@@ -30,7 +33,6 @@ repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
 echo ""
 
 echo "Cloning dependecy repos"
-[ ! -d ./treble_patches ] && git clone https://github.com/ponces/treble_patches -b eleven
 [ ! -d ./sas-creator ] && git clone https://github.com/AndyCGYan/sas-creator
 rm -rf treble_app && git clone https://github.com/phhusson/treble_app
 
@@ -38,83 +40,33 @@ echo "Setting up build environment"
 source build/envsetup.sh &> /dev/null
 echo ""
 
-echo "Reverting LOS FOD implementation"
-cd frameworks/base
-git am $BL/patches/0001-Squashed-revert-of-LOS-FOD-implementation.patch
-cd ../..
-cd frameworks/native
-git revert 542069c3aa6d003887b4abba3c0f494e8271085f --no-edit # surfaceflinger: Add support for extension lib
-cd ../..
-echo ""
-
 echo "Applying PHH patches"
 rm -f device/*/sepolicy/common/private/genfs_contexts
 cd device/phh/treble
-git clean -fdx
 cp $BL/pe.mk .
 bash generate.sh pe
 cd ../../..
-bash ~/treble_experimentations/apply-patches.sh treble_patches
+bash $BL/apply-patches.sh $BL phh $BRANCH
 echo ""
 
-echo "Applying universal patches"
-cd build/make
-git am $BL/patches/0001-Make-broken-copy-headers-the-default.patch
-cd ../..
-cd frameworks/base
-git am $BL/patches/0001-UI-Disable-wallpaper-zoom.patch
-git am $BL/patches/0001-Disable-vendor-mismatch-warning.patch
-cd ../..
-cd vendor/aosp
-git am $BL/patches/0001-vendor_lineage-Log-privapp-permissions-whitelist-vio.patch
-cd ../..
+echo "Applying personal patches"
+bash $BL/apply-patches.sh $BL personal $BRANCH
 echo ""
 
 echo "Applying GSI-specific patches"
 cd bootable/recovery
 git revert c9a3611b0bab1744b3c4321e728c917fcdc2abc3 --no-edit # recovery: Allow custom bootloader msg offset in block misc
 cd ../..
-cd build/make
-git am $BL/patches/0001-build-fix-device-name.patch
-cd ../..
-cd device/phh/treble
-git am $BL/patches/0001-Remove-fsck-SELinux-labels.patch
-git am $BL/patches/0001-base-provide-libnfc-nci.patch
-git am $BL/patches/0001-base-remove-securize-script.patch
-git am $BL/patches/0001-board-add-broken-duplicate-rules-flag.patch
-git am $BL/patches/0001-rw-system-set-fingerprint-props.patch
-git am $BL/patches/0001-add-offline-charger-sepolicy.patch
-git am $BL/patches/0001-Add-flipendo-sepolicy.patch
-cd ../../..
 cd frameworks/av
 git revert 72fb8d96c85fd45e85516b4023cd5116b5d5a8eb --no-edit # camera: Allow devices to load custom CameraParameter code
 cd ../..
 cd frameworks/native
+git revert 542069c3aa6d003887b4abba3c0f494e8271085f --no-edit # surfaceflinger: Add support for extension lib
 git revert 581c22f979af05e48ad4843cdfa9605186d286da --no-edit # Add suspend_resume trace events to the atrace 'freq' category.
 cd ../..
 cd packages/apps/Bluetooth
 git revert bba4192627ca9987c0128f9774d79ffb17ece2f5 --no-edit # Bluetooth: Reset packages/apps/Bluetooth to upstream
 cd ../../..
-cd system/core
-git am $BL/patches/0001-Revert-init-Add-vendor-specific-initialization-hooks.patch
-git am $BL/patches/0001-Panic-into-recovery-rather-than-bootloader.patch
-git am $BL/patches/0001-Restore-sbin.patch
-git am $BL/patches/0001-fix-offline-charger-v7.patch
-cd ../..
-cd system/hardware/interfaces
-git am $BL/patches/0001-Revert-system_suspend-start-early.patch
-cd ../../..
-cd system/sepolicy
-git am $BL/patches/0001-Revert-sepolicy-Relabel-wifi.-properties-as-wifi_pro.patch
-cd ../..
-cd treble_app
-git am $BL/patches/0001-Remove-securize-preference.patch
-git am $BL/patches/0001-Remove-Customization-page.patch
-cd ..
-cd vendor/aosp
-git am $BL/patches/0001-build_soong-Disable-generated_kernel_headers.patch
-git am $BL/patches/0001-build-fix-build-number.patch
-cd ../..
 echo ""
 
 echo "Applying GSI-specific fixes"
@@ -144,20 +96,20 @@ buildSasImage() {
     case $1 in
     "treble_a64_bvN")
         bash lite-adapter.sh 32 $OUT/system.img
-        xz -c s.img -T0 > ~/builds/PixelExperience_arm32_binder64-ab-vndklite-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
-        xz -c $OUT/system.img -T0 > ~/builds/PixelExperience_arm32_binder64-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c s.img -T0 > ~/builds/$BUILD_arm32_binder64-ab-vndklite-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c $OUT/system.img -T0 > ~/builds/$BUILD_arm32_binder64-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
         ;;
     "treble_arm_bvN")
         bash run.sh 32 $OUT/system.img
-        xz -c s.img -T0 > ~/builds/PixelExperience_arm-aonly-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
-        xz -c $OUT/system.img -T0 > ~/builds/PixelExperience_arm-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c s.img -T0 > ~/builds/$BUILD_arm-aonly-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c $OUT/system.img -T0 > ~/builds/$BUILD_arm-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
         ;;
     "treble_arm64_bvN")
         bash run.sh 64 $OUT/system.img
-        xz -c s.img -T0 > ~/builds/PixelExperience_arm64-aonly-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c s.img -T0 > ~/builds/$BUILD_arm64-aonly-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
         bash lite-adapter.sh 64 $OUT/system.img
-        xz -c s.img -T0 > ~/builds/PixelExperience_arm64-ab-vndklite-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
-        xz -c $OUT/system.img -T0 > ~/builds/PixelExperience_arm64-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c s.img -T0 > ~/builds/$BUILD_arm64-ab-vndklite-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
+        xz -c $OUT/system.img -T0 > ~/builds/$BUILD_arm64-ab-11.0-$BUILD_DATE-UNOFFICIAL.img.xz
         ;;
     esac
     rm -rf s.img
@@ -175,7 +127,7 @@ buildTrebleApp
 buildVariant treble_arm_bvN
 buildVariant treble_a64_bvN
 buildVariant treble_arm64_bvN
-ls ~/builds | grep 'PixelExperience'
+ls ~/builds | grep $BUILD
 
 END=`date +%s`
 ELAPSEDM=$(($(($END-$START))/60))
