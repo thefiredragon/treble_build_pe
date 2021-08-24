5bef542099f1b6db121d56845d0f5f42a6a7d867
Alberto Ponces <ponces26@gmail.com>
2021-08-24 16:01:02 +0100

    Fix phh's patches and remove unused ones

--- apply-patches.sh
+++ apply-patches.sh
@@ -13,22 +13,9 @@ for b in $branches;do
 		[ "$p" == treble/app ] && p=treble_app
 		pushd $p
 		for patch in $patches/patches/$tree/$b/$project/*.patch;do
-			#Check if patch is already applied
-			if patch -f -p1 --dry-run -R < $patch > /dev/null;then
-				continue
-			fi
-
-			if git apply --check $patch;then
-				git am $patch
-			elif patch -f -p1 --dry-run < $patch > /dev/null;then
-				#This will fail
-				git am $patch || true
-				patch -f -p1 < $patch
-				git add -u
-				git am --continue
-			else
-				echo "Failed applying $patch"
-			fi
+			git clean -q -fdx
+			git reset -q --hard
+			git am $patch || exit
 		done
 		popd
 	done
