06f57f13ae32f2ee92465d4be2a14bd986f4a79e
Alberto Ponces <ponces26@gmail.com>
2021-12-25 03:09:32 +0000

    Add BT QTI revert patch

--- pe.mk
+++ pe.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/BoardConfigSoong.mk)
 $(call inherit-product, device/custom/sepolicy/common/sepolicy.mk)
 -include vendor/aosp/build/core/config.mk
 TARGET_BOOT_ANIMATION_RES := 1080
