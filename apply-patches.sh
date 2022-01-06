2065bcafa058c1ba40efac92b5c19d2966d806a4
thefiredragon <danny.faller@googlemail.com>
2022-01-06 10:15:19 +0100

    improve apply-patches.sh

--- apply-patches.sh
+++ apply-patches.sh
@@ -12,7 +12,22 @@ for project in $(cd $patches/patches/$tree; echo *);do
 	[ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
 	pushd $p
 	for patch in $patches/patches/$tree/$project/*.patch;do
-		git am $patch || exit
+		#Check if patch is already applied
+		if patch -f -p1 --dry-run -R < $patch > /dev/null;then
+			continue
+		fi
+
+		if git apply --check $patch;then
+			git am $patch
+		elif patch -f -p1 --dry-run < $patch > /dev/null;then
+			#This will fail
+			git am $patch || true
+			patch -f -p1 < $patch
+			git add -u
+			git am --continue
+		else
+			echo "Failed applying $patch"
+		fi
 	done
 	popd
 done
