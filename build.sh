c1e4531325d3b9ea6583bf2c8f9ca66c2b009769
thefiredragon <danny.faller@googlemail.com>
2022-05-06 12:55:41 +0200

    Sync with phh 413 Add Samsung spen overlay

--- build.sh
+++ build.sh
@@ -102,7 +102,7 @@ buildVndkliteVariant() {
 
 generatePackages() {
     xz -cv $BD/system-treble_arm64_bvS.img -T0 > $BD/"$BUILD"_arm64-ab-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
-    xz -cv $BD/system-treble_arm64_bvS-vndklite.img -T0 > $BD/"$BUILD"_arm64-ab-vndklite-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
+  # xz -cv $BD/system-treble_arm64_bvS-vndklite.img -T0 > $BD/"$BUILD"_arm64-ab-vndklite-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
     xz -cv $BD/system-treble_arm64_bvS-slim.img -T0 > $BD/"$BUILD"_arm64-ab-slim-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
     rm -rf $BD/system-*.img
 }
