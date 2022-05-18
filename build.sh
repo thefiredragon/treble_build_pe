275d7c188a0503ee31dece33c152b652729324ac
thefiredragon <danny.faller@googlemail.com>
2022-05-18 18:05:08 +0200

    sync with ponces repo

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ export CCACHE_SIZE=100G
 export CCACHE_EXEC=/usr/bin/ccache
 export CCACHE_DIR=/mnt/cache
 
+set -e
+
 START=`date +%s`
 BUILD_DATE="$(date +%Y%m%d)"
 
@@ -16,100 +18,102 @@ BRANCH=$1
 [ "$BRANCH" == "twelve" ] && BUILD="PixelExperience" || BUILD="PixelExperience_Plus"
 [ "$BRANCH" == "twelve" ] && PEMK="$BL/pe.mk" || PEMK="$BL/peplus.mk"
 
-set -eE
-trap '(\
-echo;\
-echo \!\!\! An error happened during script execution;\
-echo \!\!\! Please check console output for bad sync,;\
-echo \!\!\! failed patch application, etc.;\
-echo\
-)' ERR
-
-echo ""
-[ "$BRANCH" == "twelve" ] && echo "Pixel Experience 12 Treble Buildbot" || echo "Pixel Experience Plus 12 Treble Buildbot"
-echo "ATTENTION: this script syncs repo on each run"
-echo "Executing in 5 seconds - CTRL-C to exit"
-echo ""
-sleep 5
-
-if [ ! -d .repo ]
-then
+initRepos() {
     echo "Initializing PE workspace"
-    repo init -u https://github.com/thefiredragon/manifest -b $BRANCH
+    repo init -u https://github.com/PixelExperience/manifest -b $BRANCH
     echo ""
 
     echo "Preparing local manifest"
     mkdir -p .repo/local_manifests
     cp $BL/manifest.xml .repo/local_manifests/pixel.xml
     echo ""
-fi
+}
 
-echo "Syncing repos"
-repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
-echo ""
+syncRepos() {
+    echo "Syncing repos"
+    repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+    echo ""
+}
 
-echo "Setting up build environment"
-source build/envsetup.sh &> /dev/null
-mkdir -p $BD
-echo ""
+applyPatches() {
+    echo "Applying prerequisite patches"
+    bash $BL/apply-patches.sh $BL prerequisite $BRANCH
+    echo ""
 
-echo "Applying prerequisite patches"
-bash $BL/apply-patches.sh $BL prerequisite
-echo ""
+    echo "Applying PHH patches"
+    cd device/phh/treble
+    cp $PEMK .
+    bash generate.sh $(echo $PEMK | sed "s#$BL/##;s#.mk##")
+    cd ../../..
+    bash $BL/apply-patches.sh $BL phh $BRANCH
+    echo ""
 
-echo "Applying PHH patches"
-cd device/phh/treble
-cp $PEMK pe.mk
-bash generate.sh pe
-cd ../../..
-bash $BL/apply-patches.sh $BL phh $BRANCH
-echo ""
+    echo "Applying personal patches"
+    bash $BL/apply-patches.sh $BL personal $BRANCH
+    echo ""
+}
 
-echo "Applying personal patches"
-bash $BL/apply-patches.sh $BL personal $BRANCH
-echo ""
+setupEnv() {
+    echo "Setting up build environment"
+    source build/envsetup.sh &>/dev/null
+    mkdir -p $BD
+    echo ""
+}
 
 buildTrebleApp() {
+    echo "Building treble_app"
     cd treble_app
     bash build.sh release
     cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
     cd ..
+    echo ""
 }
 
 buildVariant() {
-    lunch treble_arm64_bvS-userdebug
+    echo "Building treble_arm64_bvN"
+    lunch treble_arm64_bvN-userdebug
     make installclean
     make -j$(nproc --all) systemimage
-    make vndk-test-sepolicy
-    mv $OUT/system.img $BD/system-treble_arm64_bvS.img
+    mv $OUT/system.img $BD/system-treble_arm64_bvN.img
+    echo ""
 }
 
 buildSlimVariant() {
+    echo "Building treble_arm64_bvN-slim"
     wget https://gist.github.com/ponces/891139a70ee4fdaf1b1c3aed3a59534e/raw/slim.patch -O /tmp/slim.patch
-    (cd vendor/gapps && git am /tmp/slim.patch)
+    (cd vendor/gapps && git am /tmp/slim.patch && rm /tmp/slim.patch)
     make -j$(nproc --all) systemimage
     (cd vendor/gapps && git reset --hard HEAD~1)
-    mv $OUT/system.img $BD/system-treble_arm64_bvS-slim.img
+    mv $OUT/system.img $BD/system-treble_arm64_bvN-slim.img
+    echo ""
 }
 
 buildVndkliteVariant() {
+    echo "Building treble_arm64_bvN-vndklite"
     cd sas-creator
-    sudo bash lite-adapter.sh 64 $BD/system-treble_arm64_bvS.img
-    cp s.img $BD/system-treble_arm64_bvS-vndklite.img
+    sudo bash lite-adapter.sh 64 $BD/system-treble_arm64_bvN.img
+    cp s.img $BD/system-treble_arm64_bvN-vndklite.img
     sudo rm -rf s.img d tmp
     cd ..
+    echo ""
 }
 
 generatePackages() {
-    xz -cv $BD/system-treble_arm64_bvS.img -T0 > $BD/"$BUILD"_arm64-ab-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
-  # xz -cv $BD/system-treble_arm64_bvS-vndklite.img -T0 > $BD/"$BUILD"_arm64-ab-vndklite-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
-    xz -cv $BD/system-treble_arm64_bvS-slim.img -T0 > $BD/"$BUILD"_arm64-ab-slim-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
+    echo "Generating packages"
+    xz -cv $BD/system-treble_arm64_bvN.img -T0 > $BD/"$BUILD"_arm64-ab-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
+  #  xz -cv $BD/system-treble_arm64_bvN-vndklite.img -T0 > $BD/"$BUILD"_arm64-ab-vndklite-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
+  #  xz -cv $BD/system-treble_arm64_bvN-slim.img -T0 > $BD/"$BUILD"_arm64-ab-slim-12.1-$BUILD_DATE-UNOFFICIAL.img.xz
     rm -rf $BD/system-*.img
+    echo ""
 }
 
+[ ! -d .repo ] && initRepos
+syncRepos
+applyPatches
+setupEnv
 buildTrebleApp
 buildVariant
-buildSlimVariant
+#buildSlimVariant
 #buildVndkliteVariant
 generatePackages
 
